7f4f4f210421a5d4b38adeadece08611b64bcb06
Clayton Richardson <car602@msstate.edu>
2018-04-11 19:17:13 -0500

    starting working on cart class

--- cart.php
+++ cart.php
@@ -1,3 +1,37 @@
-//alex was head
-$cart = "dicks";
+<?php 
+class Cart{
+    private $itemsInCart;
+    private $cartID;
+    private $cartTotal;
+    
+    public function __construct($userID){
+        $this->itemsInCart = [];
+        $this->cartID = 0;
+        $this->cartTotal = 0;
+    }
+    
+    public function deleteCart(userID){
+        require_once 'login.php';
+        $conn = new mysqli($hn, $un, $pw, $db);
+        if($conn->connect_error)
+            die($conn->connect_error);
+        $query = "DELETE FROM cart WHERE userID =". 
+            $userID;
+        
+    }
+    
+    public function getItemsInCart(){
+        return($this->itemsInCart);
+    }
+    
+    public function getCartID(){
+        return($this->itemsInCart);
+    }
+    
+    public function getCartTotal(){
+        return($this->cartTotal);
+    }
+}
 
+
+?>
