5d9a13aa76b606d78459cda96ffda44448cf0e2a
Alex <dinotrooper@gmail.com>
2018-04-24 17:44:58 -0500

    updated firstpage and searchstore

--- webpages/searchStore.php
+++ webpages/searchStore.php
@@ -389,14 +389,15 @@ img {vertical-align: middle;}
 
 <div class="header">
 
-    <a href="#.html"><img  src="../images/WhiteLogoRedo.png" alt="logo"/></a>
+    <a href="../webpages/firstPage.php"><img  src="../images/WhiteLogoRedo.png" alt="logo"/></a>
 
 
 
 <div class="topnav">
-  <a href="#">Cart</a>
-  <a href="#">Orders</a>
-  <a href="#">Account</a>
+  <a href="../webpages/titanic_logout.php">Logout</a>
+  <a href="../webpages/view_cart.php">Cart</a>
+  <a href="../webpages/view_orders.php">Orders</a>
+  <a href="../webpages/titanic_login.php">Account</a>
   <div class="search-container">
     <form method="get" action="searchStore.php">
       <input type="text" placeholder="Search..." name="search">
